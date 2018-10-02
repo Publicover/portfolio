5e8c73e4e96b813e7b107e93d4b323a20d5d55f9
Jim Publicover <jameslpublicover@gmail.com>
2018-10-01 23:31:19 -0400

    responsive images

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,11 @@
 module DashboardsHelper
+
+  def dev_time
+    distance_of_time_in_words(Date.today, Date.new(2016, 03, 30))
+  end
+
+  def poet_time
+    distance_of_time_in_words(Date.today, Date.new(2005, 07, 30))
+  end
+
 end
